b8411d7ebde07fa680c93be767b8332020bc5897
Dylan Bieber <dbieber@ycp.edu>
2020-03-15 15:11:38 -0400

    David's github isnt working on his pc. He sent the junit cases to Dylan and Dylan imported it for David.

---
+++ CS320_Group/junit/testModule/testModule.java
@@ -0,0 +1,38 @@
+package testModule;
+//package edu.ycp.cs320.lab02a_tgerst.(testModule);
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320.lab02a_tgerst.model.Module;
+
+public class testModule {
+	private Module banana;
+	private int id;
+	private String name,status;
+	
+	
+	
+	@Before
+	public void setUp() {
+		banana = new Module();
+
+		id = 7;
+		name = "emusk";
+		status = "Corona time";
+
+		banana.setId(id);
+		banana.setName(name);
+		banana.setStatus(status);
+	}
+
+	@Test
+	public void testSetId() {
+		assertEquals(banana.getId(), id);
+		assertEquals(banana.getName(), name);
+		assertEquals(banana.getStatus(), status);
+	}
+}
