7a963b6489df85abedea43d3c7545d01c3514def
mikatrost <mtrost1@ycp.edu>
2020-03-14 23:06:16 -0400

    Mikayla's commit

--- CS320_Group/main/edu/ycp/cs320/lab02a_tgerst/main/Main.java
+++ CS320_Group/main/edu/ycp/cs320/lab02a_tgerst/main/Main.java
@@ -24,6 +24,7 @@ public class Main {
 		// Inform user that server is running
 		System.out.println("RUNNING: web server on port 8081");
 		System.out.println("Git commit example");
+		System.out.println("Mikayla");
 		
         // The use of server.join() the will make the current thread join and
         // wait until the server is done executing.
