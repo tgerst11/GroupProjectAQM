0290728eb041bcf5a9641726fdc520648cc85f51
Dylan Bieber <dbieber@ycp.edu>
2020-03-15 14:38:15 -0400

    Made the Module Class w/ getters and setters

---
+++ CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/model/Module.java
@@ -0,0 +1,40 @@
+package edu.ycp.cs320.lab02a_tgerst.model;
+
+public class Module {
+	
+	private int id;
+	private String name,status;
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public void readSensorData() {
+		// TODO: Implement
+	}
+	
+	public void testData() {
+		// TODO: Implement
+	}
+	
+}
