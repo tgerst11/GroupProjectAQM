252f48ea80309d0ab580c9241a33ca98f8979fff
mikatrost <mtrost1@ycp.edu>
2020-03-16 21:30:08 -0400

    Adds to the UserSystem

--- CS320_Group/src/edu/ycp/cs320/systems/UserSystem.java
+++ CS320_Group/src/edu/ycp/cs320/systems/UserSystem.java
@@ -4,20 +4,36 @@ import edu.ycp.cs320.lab02a_tgerst.model.Admin;
 
 class UserSystem {
 	private Admin admin;
+	private String username;
+	private String password;
 	
 	public void setModel(Admin admin) {
 		this.admin = admin;
 	}
-	/*
+	
 	public boolean validateLogin() {
-		TODO: returns true if the username and password
-		 *appear in the array of existing usernames and passwords
-		 else, returns error
-		 
-		 }
+		//TODO: returns true if the username and password
+		username = admin.getUsername();
+		password = admin.getPassword();
+		
+		//search the database for the username and password
+		/*
+		 * This would consist of preparing a JDBC statement that would search the database for the username/password
+		 * if (rowsReturned==0){
+		 * return false;
+		 * }
+		 * else{
+		 * return true;
+		 * }
 		 */
+		return (Boolean) null;
+		
+		}
+		
+		 
 
 	public void createAdmin() {
+		
 		//TODO: creates a new admin account
 	}
 }
