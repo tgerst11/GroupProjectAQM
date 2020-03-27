589e7acf24fd2520ca3b03091ceb79d239558f87
Dylan Bieber <dbieber@ycp.edu>
2020-03-27 14:38:53 -0400

    Made the Data class along with its attributes

---
+++ CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/model/Data.java
@@ -0,0 +1,24 @@
+package edu.ycp.cs320.lab02a_tgerst.model;
+
+public class Data{
+	
+	private float percentHumidity;
+	
+	private float temperature;
+	
+	private float airPressure;
+	
+	private float voc;
+	
+	private float altitude;
+	
+	
+	
+	
+	
+
+	
+	
+	
+}
+
