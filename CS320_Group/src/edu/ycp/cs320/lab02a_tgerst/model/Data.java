7d47212f540582907f55e4cb91da6cbb6a19bcd8
David McHugh <dmchugh@ycp.edu>
2020-03-27 14:48:03 -0400

    Add setters and getters to Data

--- CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/model/Data.java
+++ CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/model/Data.java
@@ -12,10 +12,56 @@ public class Data{
 	
 	private float altitude;
 	
+	public Data(float percentHumidity, float temperature, float airPressure, float voc, float altitude)
+	{
+		this.percentHumidity = percentHumidity;
+		this.temperature = temperature;
+		this.airPressure = airPressure; 
+		this.voc = voc;
+		this.altitude = altitude;
+	}
 	
+	//SET
+	public void setpercentHumidity(float percentHumidity) {
+		this.percentHumidity = percentHumidity;
+	}
 	
+	public void settemperature(float temperature) {
+		this.temperature = temperature;
+	}
 	
+	public void setairPressure(float airPressure) {
+		this.airPressure = airPressure;
+	}
 	
+	public void setvoc(float voc) {
+		this.voc = voc;
+	}
+	
+	public void setaltitude(float altitude) {
+		this.altitude = altitude;
+	}
+	
+	//GET
+	public float getpercentHumidity() {
+		return percentHumidity;
+	}
+	
+	public float gettemperature() {
+		return temperature;
+	}
+	
+	public float getairPressure() {
+		return airPressure;
+	}
+	
+	public float getvoc() {
+		return voc;
+	}
+	
+	public float getaltitude() {
+		return altitude;
+	}
 
 	
 	
