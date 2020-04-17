80f9be76dfb62a85794cd868641a924e24d2fb01
Dylan Bieber <dbieber@ycp.edu>
2020-04-17 12:17:26 -0400

    Modified testData to reflect new constructor, also added console output to help debugging

--- CS320_Group/junit/testData/testData.java
+++ CS320_Group/junit/testData/testData.java
@@ -18,38 +18,76 @@ public class testData {
 	
 	@Before
 	public void setUp() {
-		percentHumidity = 66.0F;
-		temperature = 25.7f;
-		airPressure = 88.1F;
-		voc = 103.4f;
-		altitude = 12.3F;
+		percentHumidity = (float) 66.0;
+		temperature = (float) 25.7;
+		airPressure = (float) 88.1;
+		voc = (float) 103.4;
+		altitude = (float) 12.3;
+		
 		
 		banana = new Data();
+		
+		banana.setPercentHumidity(percentHumidity);
+		banana.setTemperature(temperature);
+		banana.setAirPressure(airPressure);
+		banana.setVOC(voc);
+		banana.setAltitude(altitude);
+		
 	}
+
 
 	@Test
 	public void testPercentHumidity() {
-		assertEquals(banana.getPercentHumidity(), percentHumidity, 0);
+		assertEquals(banana.getPercentHumidity(), percentHumidity,0);
+		System.out.println("\n");
+		System.out.println("Humidity");
+		System.out.println("_________");
+		System.out.println("Getter: " + banana.getPercentHumidity());
+		System.out.println("Actual: " + percentHumidity);
 	}
 	
+
 	@Test
 	public void testTemperature() {
-		assertEquals(banana.getTemperature(), temperature, 0);
+		assertEquals(banana.getTemperature(), temperature,0);
+		System.out.println("\n");
+		System.out.println("Temperature");
+		System.out.println("_________");
+		System.out.println("Getter: " + banana.getTemperature());
+		System.out.println("Actual: " + temperature);
 	}
+	
 	
 	@Test
 	public void testAirPressure() {
-		assertEquals(banana.getAirPressure(), airPressure, 0);
+		assertEquals(banana.getAirPressure(), airPressure,0);
+		System.out.println("\n");
+		System.out.println("AirPressure");
+		System.out.println("_________");
+		System.out.println("Getter: " + banana.getAirPressure());
+		System.out.println("Actual: " + airPressure);
 	}	
 	
+
 	@Test
 	public void testVoc() {
-		assertEquals(banana.getVOC(), voc, 0);
+		assertEquals(banana.getVOC(), voc,0);
+		System.out.println("\n");
+		System.out.println("VOCs");
+		System.out.println("_________");
+		System.out.println("Getter: " + banana.getVOC());
+		System.out.println("Actual: " + voc);
 	}
 	
+
 	@Test
 	public void testAltitude() {
-		assertEquals(banana.getAltitude(), altitude, 0);	
+		assertEquals(banana.getAltitude(), altitude,0 );
+		System.out.println("\n");
+		System.out.println("Altitude");
+		System.out.println("_________");
+		System.out.println("Getter: " + banana.getAltitude());
+		System.out.println("Actual: " + altitude);
 	}
 	
 }
