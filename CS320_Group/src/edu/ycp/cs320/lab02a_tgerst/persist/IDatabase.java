64a3fa4cd58bb0b8103cb78ef7c7fd592fe6fe80
Dylan Bieber <dbieber@ycp.edu>
2020-04-20 20:35:11 -0400

    Added findAllModules to the IDatabase class

--- CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/persist/IDatabase.java
+++ CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/persist/IDatabase.java
@@ -8,4 +8,8 @@ import edu.ycp.cs320.lab02a_tgerst.model.Admin;
 public interface IDatabase {
 
 	List<Admin> validateCred(String username, String password);		
+	
+	List<Module> findAllModules();
+	
+	
 }
