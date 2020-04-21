e4dafef9e7fcfab64cf0222432e8a0a78aca9cd3
Dylan Bieber <dbieber@ycp.edu>
2020-04-20 20:33:05 -0400

    Implemented AllModulesQuery

--- CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/moduleDB/AllModulesQuery.java
+++ CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/moduleDB/AllModulesQuery.java
@@ -1,14 +1,39 @@
 package edu.ycp.cs320.lab02a_tgerst.moduleDB;
 
+import java.util.List;
+import java.util.Scanner;
+
+import edu.ycp.cs320.lab02a_tgerst.persist.DerbyDatabase;
+import edu.ycp.cs320.lab02a_tgerst.model.Module;
+import edu.ycp.cs320.lab02a_tgerst.persist.InitialData;
+import edu.ycp.cs320.lab02a_tgerst.persist.DatabaseProvider;
+import edu.ycp.cs320.lab02a_tgerst.persist.IDatabase;
+
 public class AllModulesQuery{
-	
-	
-	
-	//hello
-	
-	
-	
-	
+	public static void main(String[] args) throws Exception {
+
+		// Create the default IDatabase instance
+		
+		DatabaseProvider.setInstance(new DerbyDatabase());
+		
+		
+		// get the DB instance and execute transaction
+		IDatabase db = DatabaseProvider.getInstance();
+		List<Module> moduleList = db.findAllModules();
+		
+		// check if anything was returned and output the list
+		if (moduleList.isEmpty()) {
+			System.out.println("There are no modules in the database");
+		}
+		else {
+			for (Module module : moduleList) {
+				//print the tings here 
+				
+				
+				
+			}
+		}
+	}
 }
 
 
