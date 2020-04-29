11f6c609eea2761bf6a59c9b29d0f1f2597675c3
David McHugh <dmchugh@ycp.edu>
2020-04-29 11:53:26 -0400

    added a class to allow the admins to collect data in the background

--- CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/moduleDB/AddData.java
+++ CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/moduleDB/AddData.java
@@ -41,4 +41,38 @@ public class AddData {
 		}
 		
 	}//end of main
+	
+	@SuppressWarnings("resource")
+	public static String addData(int i) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		Module mod = new Module();
+		
+		apiParseData controller = new apiParseData();
+		controller.setModel(mod);
+		
+		try {
+			controller.call();
+			} 
+			catch (Exception e){
+				
+				return "There has been an error populating your model";
+			}
+		// Create the default IDatabase instance
+		InitDatabase.init(i);
+		
+		IDatabase db = DatabaseProvider.getInstance();
+		
+		Integer location_id = db.insertData(mod.getCity(), mod.getCoordinates(), mod.getState(), mod.getCountry(), mod.getTime(), mod.getTemp(), mod.getPressure(), mod.getHumidity(), mod.getWindSpeed(), mod.getWindDir(), mod.getAQI(), mod.getMainPol());
+		
+		if (location_id > 0)
+		{
+			return "Successful addition of data.";
+		}
+		else
+		{
+			return "Failed to insert data.";			
+		}
+		
+	}
 }	
--- CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/persist/InitDatabase.java
+++ CS320_Group/src/edu/ycp/cs320/lab02a_tgerst/persist/InitDatabase.java
@@ -18,4 +18,16 @@ public class InitDatabase {
 			throw new IllegalArgumentException("Invalid choice: " + which);
 		}
 	}
+	
+	public static void init(int which) {
+		//System.out.print("Which database (0=fake, 1=derby): ");
+		//int which = Integer.parseInt(keyboard.nextLine());
+		if (which == 0) {
+			DatabaseProvider.setInstance(new FakeDatabase());
+		} else if (which == 1) {
+			DatabaseProvider.setInstance(new DerbyDatabase());
+		} else {
+			throw new IllegalArgumentException("Invalid choice: " + which);
+		}
+	}
 }
